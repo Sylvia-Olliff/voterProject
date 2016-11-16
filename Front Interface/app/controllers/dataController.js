4523c148a3f5cb7ef93f63730b81f37784299877
Joesph Olliff <joeolliff@gmail.com>
2016-11-15 20:05:55 -0500

    there will be lots of these

--- Front Interface/app/controllers/dataController.js
+++ Front Interface/app/controllers/dataController.js
@@ -5,10 +5,10 @@ var conn   		 = mysql.createConnection(connData)
 module.exports = {
 	getElectionData: function(req, res, next) {
 		conn.connect();
-		conn.query("SELECT 1 + 1 as TEST", function(err, rows, fields) {
+		conn.query("SELECT * FROM test_table", function(err, rows, fields) {
 			if (err) { throw err;}
 
-			console.log(rows[0].TEST);
+			console.log(rows[0]);
 			console.log(fields);
 
 			conn.end();
