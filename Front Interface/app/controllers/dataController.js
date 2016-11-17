dbcf4cfa9519291569f04037f964bef4e80d9273
Joesph Olliff <joeolliff@gmail.com>
2016-11-17 17:46:30 -0500

    adding actual data connection

--- Front Interface/app/controllers/dataController.js
+++ Front Interface/app/controllers/dataController.js
@@ -5,7 +5,7 @@ var conn   		 = mysql.createConnection(connData)
 module.exports = {
 	getElectionData: function(req, res, next) {
 		conn.connect();
-		conn.query("SELECT * FROM test_table", function(err, rows, fields) {
+		conn.query("SELECT * FROM elections", function(err, rows, fields) {
 			if (err) { throw err;}
 
 			console.log(rows[0]);
