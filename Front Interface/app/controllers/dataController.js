a0975789026bf3db18a7d7355e63d7b83592e9be
Joesph Olliff <joeolliff@gmail.com>
2016-11-17 21:37:50 -0500

    Async Functions
    Adding in async functions for paired position and candidate data.

--- Front Interface/app/controllers/dataController.js
+++ Front Interface/app/controllers/dataController.js
@@ -9,21 +9,34 @@ module.exports = {
 	getElectionData: function(req, res, next) {
 		conn.connect();
 		//This will be a join of all four table (or some combination therein)
-		conn.query("SELECT * FROM elections", function(err, rows, fields) {
+		conn.query("SELECT * FROM elections WHERE E_FLAG=1", function(err, rows, fields) {
 			if (err) { throw err;}
 
-			console.log(rows);
 			conn.end();
+			var electionID = rows[0].E_ID;
+			var ballotKey = rows[0].B_KEY;
+			conn.connect();
+			conn.query("SELECT B_ID, P_ID, C_ID FROM ballot WHERE B_KEY=" + ballotKey, function(err, rows, fields){
+				if(err) {throw err;}
 
-			// for(entry in rows) {
-			// 	var electionData = electionDataMold;
-			// 	electionData.positon = entry.P_NAME;
-			// 	electionData.firstName = entry.C_FIRSTNAME;
-			// 	electionData.lastName = entry.C_LASTNAME;
-			// 	electionData.party = entry.C_AFFILIATION;
+				conn.end();
+				var positionIDs = [];
+				var candidateIDs = [];
+				var ballotIDs = [];
 
-			// 	electionDataSets.push(electionData);
-			// }
+				for(entry in rows) {
+					positionIDs.push(entry.P_ID);
+					candidateIDs.push(entry.C_ID);
+					ballotIDs.push(entry.B_ID);
+				}
+
+				var ballotDataSet = {
+					positions: positionIDs,
+					candidates: candidateIDs,
+					ballots: ballotIDs
+				};
+			});
+
 
 			req.election = {
 				data: electionDataSets
@@ -31,4 +44,15 @@ module.exports = {
 			next();
 		});
 	}
+}
+
+function candidateDataQuery(P_ID, C_ID, E_ID) {
+	conn.connect();
+
+	conn.query("SELECT C_LAST_NAME, C_FIRST_NAME, C_AFFILIATION FROM candidates WHERE C_ID=" + C_ID, function(err, rows, fields) {
+		if(err) {throw err;}
+
+		//build out the async function set, that will capture paired position 
+		// and candidate data. 
+	});
 }
