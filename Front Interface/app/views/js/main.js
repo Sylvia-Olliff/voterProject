d2ed20877d70bd7828b152d129c0eeb8853c3b6a
Joesph Olliff <joeolliff@gmail.com>
2016-11-29 19:36:16 -0500

    more stupid

--- Front Interface/app/views/js/main.js
+++ Front Interface/app/views/js/main.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
 	$("#start").on('click', function(evt){
 		evt.preventDefault();
 
-		window.location("192.168.1.13:8888/elections");
+		window.location.assign("192.168.1.13:8888/elections");
 	});
 });
