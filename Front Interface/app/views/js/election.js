fe547c641f7d762cff0f63d836344e1aa80dac2e
Joesph Olliff <joeolliff@gmail.com>
2016-11-29 20:29:03 -0500

    Simplifying

--- Front Interface/app/views/js/election.js
+++ Front Interface/app/views/js/election.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	$.ajax({
-			type: "GET",
-			dataType: "HTML",
-			url: "electionData",
-			cache: false,
-			success: function(result) {
-				$("#fedData").html(result);
+		type: "GET",
+		dataType: "HTML",
+		url: "electionData",
+		cache: false,
+		success: function(result) {
+			$("#fedData").html(result);
 
-			},
-			error: function(jqXHR, status, err) {
-				console.log("Error");
-				console.log(status, err);	
-			}
-		});		
+		},
+		error: function(jqXHR, status, err) {
+			console.log("Error");
+			console.log(status, err);	
+		}
+	});		
 });
