5327e6c8a9f62dc3cdc40a61370a4f1963264463
Erik-Cheung <erikcheung3@gmail.com>
2016-12-01 18:15:27 -0500

    take 2 was a fail so take 3

--- Front Interface/app/views/js/election.js
+++ Front Interface/app/views/js/election.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 			                	}
 			                });
 
-			            };
+			            },
 			            action: function(dialogItself){
                     		dialogItself.close();
                 		}
